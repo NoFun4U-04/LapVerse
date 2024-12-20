d5d44c6b34268adf534575f79d3d7a66d541041e
DieuLinh-04 <25a4041864@hvnh.edu.vn>
2024-12-20 23:11:20 +0700

    sửa popup nè

--- js/popup.js
+++
@@ -1,9 +0,0 @@
-// Lấy phần tử popup và nút đóng
-    const popupOverlay = document.getElementById('popupOverlay');
-    const closePopup = document.getElementById('closePopup');
-
-// Gán sự kiện click cho nút đóng
-    closePopup.addEventListener('click', function() {
-        popupOverlay.style.display = 'none'; 
-    });
-
